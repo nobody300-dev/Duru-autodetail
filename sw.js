8132f423cf4bbb896f3f3643938f41d5f8ab03c0
nobody300-dev <milesinfo482@gmail.com>
2026-07-24 15:37:35 +0300

    Update service worker to version 2 with caching improvements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'duru-v1';
+const CACHE = 'duru-v2';
 const ASSETS = [
   'https://duru-autodetail.pages.dev/',
   'https://duru-autodetail.pages.dev/index.html',
@@ -23,6 +23,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
